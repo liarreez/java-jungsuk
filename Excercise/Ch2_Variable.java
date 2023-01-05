a43686d53744421f0fc652010160bafa57eaa5d9
liarreez <112878394+liarreez@users.noreply.github.com>
2023-01-05 16:15:09 +0900

    Update Ch2_Variable.java

--- Excercise/Ch2_Variable.java
+++ Excercise/Ch2_Variable.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class Main {
+{
     public static void main(String[] args) {
         ch2VarEx1();
         ch2VarEx2();
@@ -13,6 +11,10 @@ public class Main {
         overFlowEx();
         floatEx();
         floatToBinEx();
+        castingEx1();
+        castingEx2();
+        castingEx3();
+        castingEx4();
     }
 
     public static void ch2VarEx1() {
@@ -169,5 +171,60 @@ public class Main {
 
         System.out.printf("%f%n", f);
         System.out.printf("%X%n", i);
+    }
+
+    public static void castingEx1(){
+        double d = 85.4;
+        int score = (int)d;
+
+        System.out.println("score=" + score);
+        System.out.println("d=" + d);
+    }
+
+    public static void castingEx2(){
+        int i = 10;
+        byte b = (byte)i;
+        System.out.printf("[int -> byte] i=%d -> b=%d%n", i, b);
+
+        i = 300;
+        b = (byte)i;
+        System.out.printf("[int -> byte] i=%d -> b=%d%n", i, b);
+
+        b = 10;
+        i = (int)b;
+        System.out.printf("[byte -> int] b=%d -> i=%d%n", b, i);
+
+        b = -2;
+        i = (int)b;
+        System.out.printf("[byte -> int] b=%d -> i=%d%n", b, i);
+
+        System.out.println("i=" + Integer.toBinaryString(i));
+    }
+
+    public static void castingEx3(){
+        float f = 9.1234567f;
+        double d = 9.1234567;
+        double d2 = (double)f;
+
+        System.out.printf("f=%20.18f\n", f);
+        System.out.printf("d=%20.18f\n", d);
+        System.out.printf("d2=%20.18f\n", d2);
+    }
+
+    public static void castingEx4(){
+        int i = 91234567;
+        float f = (float)i;
+        int i2 = (int)f;
+
+        double d = (double)i;
+        int i3 = (int)d;
+
+        float f2 = 1.666f;
+        int i4 = (int)f2;
+
+        System.out.printf("i=%d\n", i);
+        System.out.printf("f=%f i2=%d\n", f, i2);
+        System.out.printf("d=%f i3=%d\n", d, i3);
+        System.out.printf("(int)%f=%d\n", f2, i4);
     }
 }
