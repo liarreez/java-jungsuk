e98da0d0f1939122d019d3f7f6d31822a8052e22
liarreez <112878394+liarreez@users.noreply.github.com>
2023-01-05 23:56:04 +0900

    Update Ch3_Operator.java

--- Excercise/Ch3_Operator.java
+++ Excercise/Ch3_Operator.java
@@ -4,6 +4,11 @@ public class Main {
         operatorEx2();
         operatorEx3();
         operatorEx4();
+        operatorEx5();
+        operatorEx7();
+        operatorEx8();
+        operatorEx9();
+        operatorEx10();
     }
 
     public static void operatorEx1(){
@@ -44,5 +49,56 @@ public class Main {
         i = -10;
         i = -i;
         System.out.println(i);
+    }
+
+    public static void operatorEx5(){
+        int a = 10;
+        int b = 4;
+
+        System.out.printf("%d + %d = %d%n", a, b, a + b);
+        System.out.printf("%d - %d = %d%n", a, b, a - b);
+        System.out.printf("%d * %d = %d%n", a, b, a * b);
+        System.out.printf("%d / %d = %d%n", a, b, a / b);
+        System.out.printf("%d / %f = %f%n", a, (float)b, a / (float)b);
+    }
+
+    public static void operatorEx6(){
+//        byte a = 10;
+//        byte b = 20;
+//        byte c = a + b; 에러 발생 byte형 변환 필요
+//        System.out.println(c);
+    }
+
+    public static void operatorEx7(){
+        byte a = 10;
+        byte b = 30;
+        byte c = (byte) (a * b);
+        System.out.println(c);
+    }
+
+    public static void operatorEx8(){
+        int a = 1_000_000;
+        int b = 2_000_000;
+
+        long c = a * b;
+        System.out.println(c);
+    }
+
+    public static void operatorEx9(){
+        long a = 1_000_000 * 1_000_000;
+        long b = 1_000_000 * 1_000_000L;
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+
+    public static void operatorEx10(){
+        int a = 1000000;
+
+        int result1 = a * a / a;
+        int result2 = a / a * a;
+
+        System.out.printf("%d * %d / %d = %d%n", a, a, a, result1);
+        System.out.printf("%d / %d * %d = %d%n", a, a, a, result2);
     }
 }
