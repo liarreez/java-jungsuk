1ec10dbbd3d96490a3022ff39d2e85987322a0a4
liarreez <112878394+liarreez@users.noreply.github.com>
2023-01-06 21:18:41 +0900

    Update Ch3_Operator.java

--- Excercise/Ch3_Operator.java
+++ Excercise/Ch3_Operator.java
@@ -28,6 +28,9 @@ public class Main {
         operatorEx25();
         operatorEx26();
         operatorEx27();
+        operatorEx28();
+        operatorEx29();
+        operatorEx30();
     }
 
     public static void operatorEx1(){
@@ -321,5 +324,59 @@ public class Main {
         System.out.printf("ch<'a' || ch>'z'=%b%n", ch < 'a' || ch > 'z');
         System.out.printf("!('a'<=ch && ch<='z')=%b%n", !('a'<=ch && ch<='z'));
         System.out.printf(" 'a'<=ch && ch<='z'=%b%n", 'a'<=ch && ch<='z');
+    }
+
+    public static void operatorEx28(){
+        int x = 0xAB, y = 0xF;
+
+        System.out.printf("x = %#X \t\t%s%n", x, toBinaryString(x));
+        System.out.printf("y = %#X \t\t%s%n", y, toBinaryString(y));
+        System.out.printf("%#X | %#X = %#X \t%s%n", x, y, x | y, toBinaryString(x | y));
+        System.out.printf("%#X & %#X = %#X \t%s%n", x, y, x & y, toBinaryString(x & y));
+        System.out.printf("%#X ^ %#X = %#X \t%s%n", x, y, x ^ y, toBinaryString(x ^ y));
+        System.out.printf("%#X ^ %#X ^ %#X = %#X %s%n", x, y, y, x ^ y ^ y, toBinaryString(x ^ y ^ y));
+
+    }
+    static String toBinaryString(int x) {
+        String zero = "00000000000000000000000000000000";
+        String tmp = zero + Integer.toBinaryString(x);
+        return tmp.substring(tmp.length() - 32);
+    }
+
+    public static void operatorEx29(){
+        byte p = 10;
+        byte n = -10;
+
+        System.out.printf(" p =%d \t%s%n", p, toBinaryString(p));
+        System.out.printf(" ~p =%d \t%s%n", ~p, toBinaryString(~p));
+        System.out.printf(" ~p+1 =%d \t%s%n", ~p+1, toBinaryString(~p+1));
+        System.out.printf(" ~~p =%d \t%s%n", ~~p, toBinaryString(~~p));
+        System.out.println();
+        System.out.printf(" n =%d%n", n);
+        System.out.printf("~(n-1)=%d%n", ~(n-1));
+    }
+
+    public static void operatorEx30(){
+        int dec = 8;
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 1, dec >> 1, toBinaryString(dec >> 1));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 2, dec >> 2, toBinaryString(dec >> 2));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 0, dec << 0, toBinaryString(dec << 0));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 1, dec << 1, toBinaryString(dec << 1));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 2, dec << 2, toBinaryString(dec << 2));
+        System.out.println();
+
+        dec = -8;
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 1, dec >> 1, toBinaryString(dec >> 1));
+        System.out.printf("%d >> %d = %4d \t%s%n", dec, 2, dec >> 2, toBinaryString(dec >> 2));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 0, dec << 0, toBinaryString(dec << 0));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 1, dec << 1, toBinaryString(dec << 1));
+        System.out.printf("%d << %d = %4d \t%s%n", dec, 2, dec << 2, toBinaryString(dec << 2));
+        System.out.println();
+
+        dec = 8;
+        System.out.printf("%d >> %2d = %4d \t%s%n", dec, 0, dec >> 0, toBinaryString(dec >> 0));
+        System.out.printf("%d >> %2d = %4d \t%s%n", dec, 32, dec >> 32, toBinaryString(dec >> 32));
     }
 }
