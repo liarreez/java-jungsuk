8c043beb49d9c0cbbda6fe74e4ec21049e462bbd
liarreez <112878394+liarreez@users.noreply.github.com>
2023-01-08 04:39:55 +0900

    Update Ch4_Flow.java

--- Excercise/Ch4_Flow.java
+++ Excercise/Ch4_Flow.java
@@ -24,6 +24,18 @@ public class Main {
         flowEx20();
         flowEx21();
         flowEx22();
+        flowEx23();
+        flowEx24();
+        flowEx25();
+        flowEx26();
+        flowEx27();
+        flowEx28();
+        flowEx29();
+        flowEx30();
+        flowEx31();
+        flowEx32();
+        flowEx33();
+        flowEx34();
     }
 
     public static void flowEx1() {
@@ -390,5 +402,217 @@ public class Main {
         System.out.println();
         System.out.println("sum="+sum);
 
+    }
+
+    public static void flowEx23() {
+        int i =5;
+
+        while (i--!=0) {
+            System.out.println(i + " - I can do it.");
+        }
+    }
+
+    public static void flowEx24() {
+        int i = 11;
+
+        System.out.println("카운트 다운을 시작합니다.");
+
+        while(i--!=0) {
+            System.out.println(i);
+
+            for (int j=0;j<2_000_000_000;j++) {
+                ;
+            }
+        }
+        System.out.println("GAME OVER");
+    }
+
+    public static void flowEx25() {
+        int num = 0, sum = 0;
+        System.out.print("숫자를 입력하세요. (예:12345)>");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        num = Integer.parseInt(tmp);
+
+        while(num!=0) {
+            sum += num%10;
+            System.out.printf("sum=%3d num=%d%n", sum, num);
+
+            num /= 10;
+        }
+        System.out.println("각 자리수의 합:"+sum);
+    }
+
+    public static void flowEx26() {
+        int sum = 0;
+        int i = 0;
+
+        while((sum += ++i) <= 100) {
+            System.out.printf("%d - %d%n",i, sum);
+        }
+    }
+
+    public static void flowEx27() {
+        int num;
+        int sum = 0;
+        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("합계를 구할 숫자를 입력하세요.(끝내려면 0을 입력)");
+
+        while(flag) {
+            System.out.print(">>");
+
+            String tmp = scanner.nextLine();
+            num = Integer.parseInt(tmp);
+
+            if(num!=0) {
+                sum += num;
+            } else {
+                flag = false;
+            }
+        }
+        System.out.println("합계:"+sum);
+    }
+
+    public static void flowEx28() {
+        int input = 0, answer = 0;
+
+        answer = (int) (Math.random()*100) + 1;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("1과 100사이의 정수를 입력하세요.>");
+            input = scanner.nextInt();
+            if (input > answer) {
+                System.out.println("더 작은 수로 다시 시도해보세요.");
+            } else if (input < answer) {
+                System.out.println("더 큰 수로 다시 시도해보세요.");
+            }
+        } while (input!=answer);
+        System.out.println("정답입니다.");
+    }
+
+    public static void flowEx29() {
+        for(int i=1;i<=100;i++) {
+            System.out.printf("i=%d", i);
+
+            int tmp = i;
+
+            do {
+                if (tmp % 10 % 3 == 0 && tmp % 10 != 0)
+                    System.out.print("짝");
+            } while((tmp/=10)!=0);
+            System.out.println();
+        }
+    }
+
+    public static void flowEx30() {
+        int sum = 0;
+        int i = 0;
+
+        while(true) {
+            if(sum > 100)
+                break;
+            ++i;
+            sum += i;
+        }
+        System.out.println("i=" + i);
+        System.out.println("sum=" + sum);
+    }
+
+    public static void flowEx31() {
+        for(int i=0;i<=10;i++) {
+            if (i%3==0)
+                continue;
+            System.out.println(i);
+        }
+    }
+
+    public static void flowEx32() {
+        int menu = 0;
+        int num = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+            System.out.print("원하는 메뉴 (1~3)를 선택하세요.(종료:0)>");
+
+            String tmp = scanner.nextLine();
+            menu = Integer.parseInt(tmp);
+
+            if(menu==0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (!(1 <= menu && menu <= 3)) {
+                System.out.println("메뉴를 잘못 선택하셨습니다.(종료는 0)");
+                continue;
+            }
+
+            System.out.println("선택하신 메뉴는 " + menu + "번입니다.");
+        }
+    }
+
+    public static void flowEx33() {
+        Loop1 : for(int i=2;i<=9;i++) {
+            for(int j=1;j<=9;j++) {
+                if(j==5)
+                    break Loop1;
+//                    break;
+//                    continue Loop1;
+//                    continue;
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void flowEx34() {
+        int menu = 0, num = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        outer:
+        while(true) {
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+            System.out.print("원하는 메뉴 (1~3)를 선택하세요.(종료:0)>");
+
+            String tmp = scanner.nextLine();
+            menu = Integer.parseInt(tmp);
+
+            if(menu==0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (!(1 <= menu && menu <= 3)) {
+                System.out.println("메뉴를 잘못 선택하셨습니다.(종료는 0)");
+                continue;
+            }
+
+            for(;;) {
+                System.out.print("계산할 값을 입력하세요.(계산 종료:0, 전체 종료:99)>");
+                tmp = scanner.nextLine();
+                num = Integer.parseInt(tmp);
+
+                if(num==0)
+                    break;
+                if(num==99)
+                    break outer;
+                switch ((menu)) {
+                    case 1:
+                        System.out.println("result="+ num*num);
+                        break;
+                    case 2:
+                        System.out.println("result="+ Math.sqrt(num));
+                        break;
+                    case 3:
+                        System.out.println("result="+Math.log(num));
+                }
+            }
+        }
     }
 }
