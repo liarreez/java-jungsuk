a99fb3ab8c47167a47670fcd286c1344f5e6c87d
liarreez <112878394+liarreez@users.noreply.github.com>
2023-01-07 20:04:28 +0900

    Update Ch4_Flow.java

--- Excercise/Ch4_Flow.java
+++ Excercise/Ch4_Flow.java
@@ -7,6 +7,12 @@ public class Main {
         flowEx3();
         flowEx4();
         flowEx5();
+        flowEx6();
+        flowEx7();
+        flowEx8();
+        flowEx9();
+        flowEx10();
+        flowEx11();
     }
 
     public static void flowEx1() {
@@ -107,4 +113,158 @@ public class Main {
         }
         System.out.printf("당신의 학점은 %c%c입니다.", grade, opt);
     }
+
+    public static void flowEx6() {
+        System.out.print("현재 월을 입력하세요.>");
+
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+
+        switch (month) {
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("현재의 계절은 봄입니다.");
+                break;
+            case 6: case 7: case 8:
+                System.out.println("현재의 계절은 여름입니다.");
+                break;
+            case 9: case 10: case 11:
+                System.out.println("현재의 계절은 가을입니다.");
+                break;
+            default:
+            case 12: case 1: case 2:
+                System.out.println("현재의 계절은 겨울입니다.");
+        }
+    }
+
+    public static void flowEx7() {
+        System.out.print("가위(1).바위(2),보(3) 중 하나를 입력하세요.>");
+
+        Scanner scanner = new Scanner(System.in);
+        int user = scanner.nextInt();
+        int com = (int) (Math.random() * 3) + 1;
+
+        System.out.println("당신은 "+ user + "입니다.");
+        System.out.println("컴은 "+ com + "입니다.");
+
+        switch (user-com) {
+            case 2: case -1:
+                System.out.println("당신이 졌습니다.");
+                break;
+            case 1: case -2:
+                System.out.println("당신이 이겼습니다.");
+                break;
+            case 0:
+                System.out.println("비겼습니다.");
+                break;
+        }
+    }
+
+    public static void flowEx8() {
+        System.out.print("당신의 주민번호를 입력하세요. (011231-1111222)>");
+
+        Scanner scanner = new Scanner(System.in);
+        String regNo = scanner.nextLine();
+
+        char gender = regNo.charAt(7);
+
+        switch (gender) {
+            case '1': case '3':
+                System.out.println("당신은 남자입니다.");
+                break;
+            case '2': case '4':
+                System.out.println("당신은 여자입니다.");
+                break;
+            default:
+                System.out.println("유효하지 않은 주민등록번호입니다.");
+        }
+    }
+
+    public static void flowEx9() {
+        char grade = ' ';
+
+        System.out.print("당신의 점수를 입력하세요. (1~100)>");
+
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+
+        switch (score) {
+            case 100: case 99: case 98: case 97: case 96:
+            case 95: case 94: case 93: case 92: case 91: case 90:
+                grade = 'A';
+                break;
+            case 89: case 88: case 87: case 86: case 85:
+            case 84: case 83: case 82: case 81: case 80:
+                grade = 'B';
+                break;
+            case 79: case 78: case 77: case 76: case 75:
+            case 74: case 73: case 72: case 71: case 70:
+                grade = 'C';
+                break;
+            default:
+                grade = 'F';
+        }
+
+        System.out.println("당신의 학점은 " + grade + "입니다.");
+    }
+
+    public static void flowEx10() {
+        int score = 0;
+        char grade = ' ';
+
+        System.out.print("당신의 점수를 입력하세요. (1~100)>");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        score = Integer.parseInt(tmp);
+
+        switch (score/10) {
+            case 10:
+            case 9:
+                grade = 'A';
+                break;
+            case 8:
+                grade = 'B';
+                break;
+            case 7:
+                grade = 'C';
+                break;
+            default:
+                grade = 'F';
+        }
+        System.out.println("당신의 학점은 " + grade + "입니다.");
+    }
+
+    public static void flowEx11() {
+        System.out.print("당신의 주민번호를 입력하세요. (011231-1111222)>");
+
+        Scanner scanner = new Scanner(System.in);
+        String regNo = scanner.nextLine();
+        char gender = regNo.charAt(7);
+
+        switch (gender) {
+            case '1': case '3':
+                switch(gender) {
+                    case '1':
+                        System.out.println("당신은 2000년 이전에 출생한 남자입니다.");
+                        break;
+                    case '3':
+                        System.out.println("당신은 2000년 이후에 출생한 남자입니다.");
+                }
+                break;
+            case '2': case '4':
+                switch(gender) {
+                    case '2':
+                        System.out.println("당신은 2000년 이전에 출생한 여자입니다.");
+                        break;
+                    case '4':
+                        System.out.println("당신은 2000년 이후에 출생한 여자입니다.");
+                }
+                break;
+            default:
+                System.out.println("유효하지 않은 주민등록번호입니다.");
+        }
+    }
+
 }
